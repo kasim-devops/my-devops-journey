18b9d06ad00d990da00633f760b58192270fbc3f
akhtarkasim3-debug <akhtarkasim3@gmail.com>
2026-07-13 21:19:00 +0100

    fix test_hello.py error

--- chapter4/test_hello.py
+++ chapter4/test_hello.py
@@ -1,7 +1,7 @@
-import unitest
+import unittest
 
 from hello import say_hello
 
 class TestHello(unittest.TestCase):
     def test_say_hello(self):
-        self.assertEqual(say_hello(), "Hello, World!")
+        self.assertEqual(say_hello(), "Hello World!")
